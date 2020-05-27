a55481f806c2ee7b313516335952b3cd9100d5b2
dahae0320 <ss7ssy@naver.com>
2020-05-27 21:50:27 +0900

    setting change

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%3x1p!$n8fy1wzq7&uj4tm@@)b&@)83^*ff-adc(b3-3pw^m_^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
