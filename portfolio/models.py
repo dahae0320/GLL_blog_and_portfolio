5f5c9251a6ebd09e3548d1fa9219d7712d134f47
dahae0320 <ss7ssy@naver.com>
2020-05-27 22:03:38 +0900

    model update

--- portfolio/models.py
+++ portfolio/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 
 class Portfolio(models.Model):
-    image = models.ImageField(upload_to='portfolio/%Y/%M/%D/')
+    image = models.ImageField(upload_to="portfolio/%Y/%m/%d")
     description = models.CharField(max_length=500)
